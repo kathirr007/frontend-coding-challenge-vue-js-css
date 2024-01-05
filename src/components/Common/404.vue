d8991c08b5acc09e22508a40fafc84a95e90ef6e
Kathiravan K <kathirr007@gmail.com>
2024-01-06 02:14:05 +0530

    core(): added 404 page

--- src/components/Common/404.vue
+++ src/components/Common/404.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { HomeIcon } from '@heroicons/vue/24/solid';
+
 withDefaults(
   defineProps<{
     title?: string;
@@ -15,36 +17,48 @@ const backgroundStyle = {
 </script>
 
 <template>
-  <!-- <div
-    class="surface-section px-4 py-8 md:px-6 lg:px-8 w-full not-found-background"
+  <main
+    class="flex align-center relative isolate w-full min-h-full px-4 py-8 md:px-6 lg:px-8 w-full not-found-background"
     :style="backgroundStyle"
   >
-    <div class="text-center">
-      <span class="font-bold text-2xl text-pink-500 px-3">404</span>
+    <div
+      class="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8"
+      :style="backgroundStyle"
+    >
+      <div class="text-center">
+        <span class="font-bold text-2xl text-pink-500 px-3">404</span>
+      </div>
+      <div class="mt-6 mb-5 font-bold text-6xl text-900 text-center">
+        {{ title || 'Page Not Found' }}
+      </div>
+      <p class="text-700 text-3xl text-primary mt-0 mb-6 text-center">
+        Sorry, we couldn't find the page.
+      </p>
+      <div class="text-center flex justify-center" v-if="!hideNavigations">
+        <a
+          class="p-button-text mr-2 font-semibold"
+          label="Go Back"
+          icon="pi pi-chevron-left"
+          @click.prevent="$router.go(-1)"
+          href="#"
+        >
+          <span aria-hidden="true">&larr;</span>
+          Go Back</a
+        >
+        <a
+          :label="'Go to Home'"
+          icon="pi pi-lock"
+          href="#"
+          class="font-semibold flex items-center justify-center gap-x-1"
+          @click.prevent="$router.push({ name: 'index' })"
+        >
+          <component :is="HomeIcon" class="h-6 w-6" aria-hidden="true" />
+          Go to Home</a
+        >
+      </div>
     </div>
-    <div class="mt-6 mb-5 font-bold text-6xl text-900 text-center">
-      {{ title || 'Page Not Found' }}
-    </div>
-    <p class="text-700 text-3xl text-primary mt-0 mb-6 text-center">
-      Sorry, we couldn't find the page.
-    </p>
-    <div class="text-center" v-if="!hideNavigations">
-      <a
-        class="p-button-text mr-2"
-        label="Go Back"
-        icon="pi pi-chevron-left"
-        @click="$router.go(-1)"
-        >Go Back</a
-      >
-      <a
-        :label="'Go to Home'"
-        icon="pi pi-lock"
-        @click="$router.push({ name: 'index' })"
-        >Go to Home</a
-      >
-    </div>
-  </div> -->
-  <main class="relative isolate w-full min-h-full">
+  </main>
+  <!-- <main class="relative isolate w-full min-h-full">
     <img
       src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
       alt=""
@@ -64,5 +78,5 @@ const backgroundStyle = {
         >
       </div>
     </div>
-  </main>
+  </main> -->
 </template>
